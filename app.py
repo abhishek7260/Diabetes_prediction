7a3abf6c693990fe0e362c840cc550191887c2f0
abhishek7260 <abhishekranjan7260@gmail.com>
2024-04-08 21:08:16 +0530

    add

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ model = pickle.load(open('diabetes.sav', 'rb'))
 
 # Page title
 st.title('Diabetes Prediction using ML')
-
 # Input fields
 col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
 
